2e65a693ec2bff7fad7055935890697fff907983
Muhammad Hakimi Shahrul Azmi <hakimishahrul@gmail.com>
2024-05-21 16:00:06 +0800

    Chapter 12 done.

---
+++ app/lib/actions.ts
@@ -0,0 +1,71 @@
+
+// Adding the Server Actions directly inside Server Components by adding "use server" inside the action.
+'use server';
+
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { z } from "zod";
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+
+// Create invoices
+export async function createInvoice(formData: FormData) {
+
+    const {customerId, amount, status} = CreateInvoice.parse({
+
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+
+    });
+    const amountInCents = amount * 10;
+    const date = new Date().toISOString().split('T')[0];
+
+
+    // Insert data into SQL
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+
+// Update Invoices
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'), 
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+// Delete invoices
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
