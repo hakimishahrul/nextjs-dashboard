64ddbe456210e4a2cc2e1a252fbb95cd0469b823
Muhammad Hakimi Shahrul Azmi <hakimishahrul@gmail.com>
2024-05-29 14:00:04 +0800

    Completing Chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@
 'use server';
 
 import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -131,4 +133,23 @@ export async function deleteInvoice(id: string) {
     }
     
     
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
